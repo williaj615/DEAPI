bacab1fa46dfd2d1b6dcb19091a70d60f717156c
Jasmine <jasminewilliams2017@gmail.com>
2020-03-31 14:31:26 -0500

    get and get by id for employees

---
+++ DE-API/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEAPI.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public List<Employee> Employees
+        {
+            get; set;
+        }
+    }
+}
